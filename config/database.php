9c74bab7a4b402de9f367588a06318867fdb740f
useless-id <stefanoelatini@hotmail.it>
2017-01-08 16:27:39 +0000

    Add Heroku creds and update default route to hit the DB.

--- config/database.php
+++ config/database.php
@@ -70,19 +70,16 @@ return [
             'engine' => null,
         ],
         */
-        'mysql' => [
-            'driver' => 'mysql',
-            'host' => $host,
-            //'port' => env('DB_PORT', '3306'),
-            'database' => $database,
-            'username' => $username,
-            'password' => $password,
-            'charset' => 'utf8',
-            'collation' => 'utf8_unicode_ci',
-            'prefix' => '',
-            'strict' => false,
-            'engine' => null,
-        ],
+        'mysql' => array(
+        'driver'    => 'mysql',
+        'host'      => $host,
+        'database'  => $database,
+        'username'  => $username,
+        'password'  => $password,
+        'charset'   => 'utf8',
+        'collation' => 'utf8_unicode_ci',
+        'prefix'    => '',
+    ),
 
         'pgsql' => [
             'driver' => 'pgsql',
