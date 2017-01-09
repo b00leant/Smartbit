7bcef63c04b29a4feae25840c643a7692f7da554
useless-id <stefanoelatini@hotmail.it>
2017-01-09 02:35:07 +0000

    ricevuta pdf update

--- public/js/sbpdf.js
+++ public/js/sbpdf.js
@@ -34,7 +34,7 @@ $('a.print-repair').on('click',function(){
     var data = can.toDataURL( "image/jpeg" );
     doc.addImage(data, 'JPEG', 80, 165);
     doc.addPage();
-    oc.text(20, 30, 'Ricevuta riparazione per Smartbit');
+    doc.text(20, 30, 'Ricevuta riparazione per Smartbit');
     doc.setFontSize(10);
     doc.text(20, 40, $('.modello-ricevuta').text()+"\n"+
     $('.marca-ricevuta').text()+
