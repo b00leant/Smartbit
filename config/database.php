a6dec267a745fc916e09066ed8a3b91ffe92d556
useless-id <stefanoelatini@hotmail.it>
2017-01-12 14:32:23 +0000

    added SMSGateway singleton!

--- config/database.php
+++ config/database.php
@@ -5,12 +5,12 @@
  $password = $url["pass"];
  $database = substr($url["path"], 1);*/
  
-/*
+
 $url = parse_url(getenv("DATABASE_URL"));
 $host = $url["host"];
 $username = $url["user"];
 $password = $url["pass"];
-$database = substr($url["path"], 1);*/
+$database = substr($url["path"], 1);
 return [
 
     /*
@@ -88,7 +88,7 @@ return [
         'collation' => 'utf8_unicode_ci',
         'prefix'    => '',
     ),*/
-    
+    /*
         'pgsql' => [
             'driver' => 'pgsql',
             'host' => env('DB_HOST', 'localhost'),
@@ -100,8 +100,8 @@ return [
             'prefix' => '',
             'schema' => 'public',
         ],
+    */
     
-    /*
     'pgsql' => array(
         'driver'   => 'pgsql',
         'host'     => $host,
@@ -111,7 +111,7 @@ return [
         'charset'  => 'utf8',
         'prefix'   => '',
         'schema'   => 'public',
-    ),*/
+    ),
     ],
     
     /*
