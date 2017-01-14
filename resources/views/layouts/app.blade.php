c6cf46e9f2ac22431951c3bc13d2cf4f38ec6b95
useless-id <stefanoelatini@hotmail.it>
2017-01-14 15:31:23 +0000

    metadata

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -457,6 +457,7 @@
 
     <!--Import jQuery before materialize.js-->
     <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.js"></script>
     <!-- Compiled and minified JavaScript -->
     <!--script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/js/materialize.min.js"></script-->
     <!-- JavaScripts -->
@@ -621,6 +622,11 @@
             
         });*/
  		$(document).ready(function(){
+ 		    
+ 		    if(!Modernizr.input.placeholder) {
+                //insert placeholder polyfill script here.
+            }
+ 		    
  		    lastEvent = void 0;
 
             heldKeys = {};
