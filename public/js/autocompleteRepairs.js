df87e8e2073262b179c863e977157bf35f7bf629
useless-id <stefanoelatini@hotmail.it>
2017-01-16 04:26:57 +0000

    added search repairs

--- public/js/autocompleteRepairs.js
+++ public/js/autocompleteRepairs.js
@@ -94,8 +94,8 @@ $.fn.autocompletesb2showREP = function(options){
                             var autocompleteOption = $('<li data-seriale="'+data[key].seriale+'" data-modello="'+data[key].modello+'" data-proprietario="'+data[key].proprietario+'"></li>');
                             autocompleteOption.append('<a href="/repair/'+data[key].id+'"><span>'+data[key].modello+' ('+data[key].seriale +')</span></a>');
                             $autocomplete.append(autocompleteOption);
+                            highlight(val, autocompleteOption);
                             i++;
-                            //highlight(val, autocompleteOption);
                         }else{
                             /*
                             var autocompleteOption = $('<li><span>Aggiungi Cliente</span></li>');
