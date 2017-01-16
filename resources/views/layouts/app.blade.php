806d271a58fc9cd073ab690cf6f248284df6043c
useless-id <stefanoelatini@hotmail.it>
2017-01-16 17:13:10 +0000

    Fixed fonts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -463,8 +463,8 @@
     <!--Import jQuery before materialize.js-->
     <!--script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script-->
     <script src="{{ asset('js/jquery/jquery-2.1.1.js') }}"></script>
-    {{-- <!--script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.js"></script--> --}}
-    {-- <!--script src="{{ asset('js/modernizr/modernizr.js') }}"></script--> --}}
+     <!--script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.js"></script--> 
+     <!--script src="{{ asset('js/modernizr/modernizr.js') }}"></script--> 
     <!-- Compiled and minified JavaScript -->
     <!--script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/js/materialize.min.js"></script-->
     <!-- JavaScripts -->
