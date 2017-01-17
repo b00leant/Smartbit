ab19711e1808aadc1be22e04920de249866ec120
useless-id <stefanoelatini@hotmail.it>
2017-01-17 01:26:47 +0000

    Fixed CSS, Layout and Business Logic for Deliveries

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -108,8 +108,8 @@
     <!--link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous"-->
     {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
 </head>
-@if(Route::getCurrentRoute()->getPath() == 'home')
-<body style="background:url('{{asset('images/seigaiha.png')}}');repeat:repeat">
+@if(Route::getCurrentRoute()->getPath() == '/home')
+<body style="background:url('{{asset('images/seigaiha.png')}}');repeat:repeat;">
 @else
 <body>
 @endif
