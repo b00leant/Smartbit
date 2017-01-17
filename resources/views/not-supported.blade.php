1c674f19389124e7c0ad0814aa0332f6b83b499f
useless-id <stefanoelatini@hotmail.it>
2017-01-17 00:08:05 +0000

    Fixed CSS, Layout and Business Logic for Deliveries

--- resources/views/not-supported.blade.php
+++ resources/views/not-supported.blade.php
@@ -1,2 +1,52 @@
-BROWSER NON SUPPORTATO ): / NOT SUPPORTED ): 
-si prega di aggiornare Internet Explorer alla versione 10 o superiore!
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Be right back.</title>
+
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+
+        <style>
+            html, body {
+                background: url('http://sf.co.ua/16/01/wallpaper-5da9.jpg');
+                height: 100%;
+                background-size: cover;
+                background-repeat: no-repeat;
+            }
+
+            body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                color: #B0BEC5;
+                display: table;
+                font-weight: 100;
+                font-family: 'Lato';
+            }
+
+            .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
+
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
+
+            .title {
+                /*color:#a3e3e8;*/
+                color:#3a1c2e;
+                font-size: 72px;
+                margin-bottom: 40px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <div class="title">BROWSER NON SUPPORTATO si prega di aggiornare Internet Explorer alla versione 10 o superiore!</div>
+            </div>
+        </div>
+    </body>
+</html>
