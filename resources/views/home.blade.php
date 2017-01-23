41ba96af481b5148a15962df5f4e7ea202c56ecb
useless-id <stefanoelatini@hotmail.it>
2017-01-23 00:35:10 +0000

    Fixed CSS HOME

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,7 +12,7 @@
     }(document, 'script', 'facebook-jssdk'));
 </script>
 
-<div class="row home" style="margin-bottom:0">
+<div class="row home" style="margin-bottom:0;padding-top:56px">
     <div class="col s12 l8" style="padding:0">
         <div class="col s12 l8" style="padding:0;position:absolute;margin-top:90px;z-index:2">
     <div class="fairyoutl " id="open" style="padding:0">
@@ -25,9 +25,9 @@
     <div class="hour-banner white-text" id="open" style="
     border:0px;background-color: rgb(6, 86, 116);font-size:1.5em">Benvenuto</div>
 </div>
-        <div id="maphome" style="width:100%;height: 585px; position: relative;"></div>
+        <div id="maphome" style="width:100%;height: 525px; position: relative;"></div>
     </div>
-    <div style="text-align:center;padding-top:75px;padding-bottom:10px" class="grey darken-3 col s12 l4">
+    <div style="text-align:center;padding-top:15px;padding-bottom:10px" class="grey darken-3 col s12 l4">
         <div class="fb-page" 
         data-href="https://www.facebook.com/smartbitsrl/" 
         data-tabs="timeline" 
