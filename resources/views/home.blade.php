49ddc54a21085c78df4d1707daead8d7b493fd65
useless-id <stefanoelatini@hotmail.it>
2017-01-23 00:23:29 +0000

    Fixed CSS HOME

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,18 +1,44 @@
 @extends('layouts.app')
 
 @section('content')
+{{-- <div id="fb-root"></div> --}}
+<script>
+(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/it_IT/sdk.js#xfbml=1&version=v2.8&appId=1696543827255530";
+    fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+</script>
+
 <div class="row home" style="margin-bottom:0">
-    <div style="position:absolute;width:100%;padding-top:90px;z-index:2">
-        <div class="fairyoutl " id="open" style="padding:0">
-        </div>
-        <div class="fairyl " id="open" style="padding:0">
-        </div>
-        <div class="fairyoutr " id="open" style="padding:0">
-        </div><div class="fairyr" id="open" style="padding:0">
-        </div>
-        <div class="hour-banner white-text" id="open" style="
-        border:0px;background-color: rgb(6, 86, 116);font-size:1.5em">Benvenuto</div>
+    <div class="col s12 l8" style="padding:0">
+        <div class="col s12 l8" style="padding:0;position:absolute;margin-top:90px;z-index:2">
+    <div class="fairyoutl " id="open" style="padding:0">
     </div>
-    <div id="maphome" style="height: 500px; width: 100%; position: relative;"></div>
+    <div class="fairyl " id="open" style="padding:0">
+    </div>
+    <div class="fairyoutr " id="open" style="padding:0">
+    </div><div class="fairyr" id="open" style="padding:0">
+    </div>
+    <div class="hour-banner white-text" id="open" style="
+    border:0px;background-color: rgb(6, 86, 116);font-size:1.5em">Benvenuto</div>
+</div>
+        <div id="maphome" style="width:100%;height: 585px; position: relative;"></div>
+    </div>
+    <div style="text-align:center;padding-top:75px;padding-bottom:10px" class="grey darken-3 col s12 l4">
+        <div class="fb-page" 
+        data-href="https://www.facebook.com/smartbitsrl/" 
+        data-tabs="timeline" 
+        data-small-header="false" 
+        data-adapt-container-width="true" 
+        data-hide-cover="true" 
+        data-show-facepile="true">
+            <blockquote cite="https://www.facebook.com/smartbitsrl/" 
+            class="fb-xfbml-parse-ignore">
+                <a href="https://www.facebook.com/smartbitsrl/">SmartBit</a></blockquote>
+        </div>
+    </div>
 </div>
 @endsection
