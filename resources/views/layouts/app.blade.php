aafd0b2829a9221aee066bc60115435ef17176e1
useless-id <stefanoelatini@hotmail.it>
2017-01-24 12:20:42 +0000

    Fixed CSS

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -831,7 +831,7 @@
 @if(Route::getCurrentRoute()->getPath() === '/')
 <footer class="page-footer" style="position:relative;
     width: 100%;
-    bottom: 0;margin-top:0;padding-top:1em;border-top: solid 0.5em #424242">
+    bottom: 0;margin-top:0;background-color:#424242;padding-top:1em;border-top: solid 0.5em #ffb300">
     <div class="container">
         <div class="home row">
   <div class="col s12 m4">
